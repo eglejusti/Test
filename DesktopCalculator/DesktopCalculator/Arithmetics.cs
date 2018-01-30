ba91af6c1bafe739dcba2933ddf116d19db7ec21
eglejusti <eglejusti@gmail.com>
2018-01-30 18:14:39 +0200

    Improvinom case IF (idejom throw)

--- DesktopCalculator/DesktopCalculator/Arithmetics.cs
+++ DesktopCalculator/DesktopCalculator/Arithmetics.cs
@@ -14,17 +14,22 @@ namespace DesktopCalculator
             switch (action)
             {
                 case "+":
-                    result = num2 + num1;
+                    result = num1 + num2;
                     break;
                 case "-":
-                    result = num2 - num1;
+                    result = num1 - num2;
                     break;
                 case "x":
-                    result = num2 * num1;
+                    result = num1 * num2;
                     break;
                 case "/":
-                    {                    {
-                        result = num2 / num1;
+                  if (num2 != 0)
+                    { 
+                        result = num1 / num2;
+                    }
+                  else
+                    {
+                        throw new DivideByZeroException();
                     }
 
                     break;
